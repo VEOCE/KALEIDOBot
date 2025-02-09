06ccb9bcebe29efc1b3054e9ce64a8aee73f7eb1
VEOCE <136567088+VEOCE@users.noreply.github.com>
2025-02-09 20:28:37 +0700

    Update banner.js

--- banner.js
+++ banner.js
@@ -3,7 +3,13 @@ import chalk from 'chalk';
 
 export function displayBanner() {
     console.log(chalk.cyan(`
-█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█ █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█ █░░║║║╠─║─║─║║║║║╠─░░█ █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█ 
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█ 
+█░░║║║╠─║─║─║║║║║╠─░░█ 
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ 
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
                    ${chalk.yellow('KALEIDO AUTO - BOT')}                
      📢  ${chalk.blue('Telegram Channel: https://t.me/FreeDropLink')}
     `));
